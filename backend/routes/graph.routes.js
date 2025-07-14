c1713a18e36927d7efdbdcfcffeb165185078574
Roger Fang <yunchengroger@gmail.com>
2025-07-14 15:49:37 -0700

    backend changes

---
+++ backend/routes/graph.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { addNode, deleteNode, addEdge, deleteEdge } = require('./graph.controller');
+const router = express.Router();
+
+router.post('/node', (req, res) => {
+  addNode(req.body);
+  res.json({ success: true });
+});
+
+router.delete('/node/:id', (req, res) => {
+  deleteNode(req.params.id);
+  res.json({ success: true });
+});
+
+router.post('/edge', (req, res) => {
+  addEdge(req.body);
+  res.json({ success: true });
+});
+
+router.delete('/edge', (req, res) => {
+  deleteEdge(req.body);
+  res.json({ success: true });
+});
+
+module.exports = router;
