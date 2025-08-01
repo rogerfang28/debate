353d0e2fd06a90ffa26a28d76dc310cc3ec7056a
Roger Fang <yunchengroger@gmail.com>
2025-08-01 16:26:58 -0700

    remove index.css for testing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css'
+// import './styles/index.css'
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);  
