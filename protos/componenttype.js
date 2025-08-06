3a4b4eaf3de362de60a2afcf58e0bb0b6fb64adf
Roger Fang <yunchengroger@gmail.com>
2025-08-05 17:10:30 -0700

    remove @bufbuild/protobuf

---
+++ protos/componenttype.js
@@ -0,0 +1,36 @@
+// source: page.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.ui.ComponentType');
+
+/**
+ * @enum {number}
+ */
+proto.ui.ComponentType = {
+  UNKNOWN: 0,
+  TEXT: 1,
+  BUTTON: 2,
+  INPUT: 3,
+  TEXTAREA: 4,
+  NODE_GRAPH: 5,
+  CHAT: 6,
+  IMAGE: 7,
+  CARD: 8,
+  LIST: 9,
+  TABLE: 10,
+  MODAL: 11,
+  CONTAINER: 12,
+  FORM: 13,
+  ICON: 14
+};
+
