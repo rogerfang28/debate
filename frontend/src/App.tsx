34c921ba8bae4c7e09f5e9c785b4d44f883de99c
Roger Fang <yunchengroger@gmail.com>
2025-08-07 17:30:39 -0700

    renderer converted to tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,24 @@
 // * Directs path to the correct page
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import GraphPage from './pages/GraphPage/GraphPage';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import PublicDebatesPage from './pages/PublicDebatesPage/PublicDebatesPage';
-import PublicDebateViewerPage from './pages/PublicDebateViewerPage/PublicDebateViewerPage';
-// @ts-ignore - Renderer.jsx not converted yet
-import Renderer from "./renderers/Renderer.jsx";
+// import HomePage from './pages/HomePage/HomePage';
+// import GraphPage from './pages/GraphPage/GraphPage';
+// import ProfilePage from './pages/ProfilePage/ProfilePage';
+// import PublicDebatesPage from './pages/PublicDebatesPage/PublicDebatesPage';
+// import PublicDebateViewerPage from './pages/PublicDebateViewerPage/PublicDebateViewerPage';
+import Renderer from "./renderers/Renderer";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Renderer />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        {/* <Route path="/profile" element={<ProfilePage />} />
         <Route path="/graph/:roomId" element={<GraphPage />} />
-        <Route path="/graph" element={<GraphPage />} /> {/* Legacy route - will redirect to home */}
+        <Route path="/graph" element={<GraphPage />} />
         <Route path="/public-debates" element={<PublicDebatesPage />} />
         <Route path="/public-debate/:roomId" element={<PublicDebateViewerPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} /> */}
       </Routes>
     </Router>
   );
